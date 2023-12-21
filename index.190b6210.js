dbdce087317d589716b7fae43adfdb05e9356817
Oleksandra Koshyk <sakoshik1984@gmail.com>
2023-12-21 18:56:40 +0100

    make build

---
+++ index.190b6210.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall");e.style.left=(t.clientWidth-e.clientWidth)/2+"px",e.style.top=(t.clientHeight-e.clientHeight)/2+"px";
+//# sourceMappingURL=index.190b6210.js.map
